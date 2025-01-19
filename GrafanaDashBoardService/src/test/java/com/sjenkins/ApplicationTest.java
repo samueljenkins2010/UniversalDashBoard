83ec913a3edfdde9294b697ec6c6a1095cc25ace
samueljenkins2010 <samueljenkins2010@hotmail.com>
2025-01-19 21:41:57 +0000

    Added Spring Boot Application to generate Dashboards for newly Discovered Services on Consul.

---
+++ GrafanaDashBoardService/src/test/java/com/sjenkins/ApplicationTest.java
@@ -0,0 +1,9 @@
+package com.sjenkins;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    public void applicationStartsUpSuccessfully() { }
+}
